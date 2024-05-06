060a7c7c7bafd626ddf5594c9fb651debb2dfbda
jon-jc <jonathancho.jc@gmail.com>
2024-05-06 16:33:39 -0700

    chore: Update tradingbot.py with symbol parameter in MLTrader's initialize method

--- tradingbot.py
+++ tradingbot.py
@@ -11,30 +11,41 @@ load_dotenv()
 
 # Retrieve API details and other configurations from environment variables
 API_KEY = os.getenv('API_KEY')
-API_KEY_SECRET = os.getenv('API_KEY_SECRET')
+API_SECRET = os.getenv('API_SECRET')
 BASE_URL = os.getenv('BASE_URL')
 
 ALPACA_CREDS = {
     'API_KEY': API_KEY,
-    'API_KEY_SECRET': API_KEY_SECRET,
+    'API_SECRET': API_SECRET,
     'PAPER': True,
 }
 
 class MLTrader(Strategy):
-    def initialize(self):
-        pass
+    def initialize(self, symbol:str="SPY"):
+        self.symbol = symbol
+        self.sleeptime = "24H"
+        self.last_trade = None
+
     def on_trading_iteration(self):
-        pass
+        if self.last_trade == None:
+            order = self.create_order(
+                self.symbol,
+                10,
+                "buy",
+                type="market"
+            )
+            self.submit_order(order)
+            self.last_trade = "buy"
 
 start_date = datetime(2023, 12, 15)
 end_date = datetime(2023, 12, 31)  # Corrected to use 'end_date' instead of the second 'start_date'
 
 broker = Alpaca(ALPACA_CREDS)
-strategy = MLTrader(name='MLTrader', broker=broker, parameters={})
+strategy = MLTrader(name='MLTrader', broker=broker, parameters={"symbol": "SPY"})
 
 strategy.backtest(
     YahooDataBacktesting,
     start_date,
     end_date,
-    parameters={}
+    parameters={"symbol": "SPY"}
 )
